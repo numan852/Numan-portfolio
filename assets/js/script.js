934158a7b306f89b307565441c394e6951435c44
numan852 <noumanahmad852@gmail.com>
2025-11-28 20:04:16 +0500

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["Student of Computer Engineer"],
+    strings: ["Student of Computer Engineering"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
